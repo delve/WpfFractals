aedadce5d0462d717b1c81e642da3492134dca3a
delve <delve202@gmail.com>
2014-09-15 19:56:46 -0400

    Fixed a snafu with render speed control. drawSpeed changed to renderTicks for tracking frames and new property DrawSpeed added for control

--- WpfFractals/WpfFractals/LineFractalWindow.xaml.cs
+++ WpfFractals/WpfFractals/LineFractalWindow.xaml.cs
@@ -8,18 +8,9 @@
 namespace WpfFractals
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
     using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
     using System.Windows.Shapes;
 
     /// <summary>
@@ -27,6 +18,19 @@ namespace WpfFractals
     /// </summary>
     public partial class LineFractalWindow : Window
     {
+        #region Fields
+        /// <summary>
+        /// Tracks how many times CompositionTarget.Rendering has fired during rendering
+        /// </summary>
+        private int renderTicks;
+
+        /// <summary>
+        /// Current fractal drawing depth
+        /// </summary>
+        private int fractalDepth;
+        #endregion
+
+        #region Constructors
         /// <summary>
         /// Initializes a new instance of the LineFractalWindow class.
         /// This window is used to draw fractals composed of lines
@@ -50,13 +54,18 @@ namespace WpfFractals
         /// </summary>
         /// <param name="minPixels">Sets the minimum size in pixels per line segment to use as the escape value for the recursion</param>
         /// <param name="depth">Sets the maximum depth to use in the recursion. 0 usually means 'until a segment is less than 1 pixel in size'</param>
-        public LineFractalWindow(int minPixels, int depth)
+        /// <param name="speed">Roughly controls the drawing speed by skipping 'speed' number of rendering cycles between depth renderings</param>
+        /// <param name="angleDelta">Child branchs' angle +/- delta from the parent</param>
+        /// <param name="childScale">Ratio of child branchs' length to parents'</param>
+        public LineFractalWindow(int minPixels, int depth = 0, int speed = 15, double angleDelta = Math.PI / 5, double childScale = 0.75)
         {
             this.InitializeComponent();
 
             this.MinSize = minPixels;
             this.MaxDepth = depth;
-            this.drawSpeed = 0;
+            this.DrawSpeed = speed;
+            this.DeltaTheta = angleDelta;
+            this.LengthScale = childScale;
             Shape shapeToRender = new Rectangle() { Fill = Brushes.Red, Height = 35, Width = 35, RadiusX = 5, RadiusY = 5 };
             Canvas.SetLeft(shapeToRender, 5);
             Canvas.SetTop(shapeToRender, 5);
@@ -64,11 +73,13 @@ namespace WpfFractals
             // Draw the shaaaape
             fractalCanvas.Children.Add(shapeToRender);
         }
+        #endregion
 
+        #region Properties
         /// <summary>
         /// Gets or sets the Brush object used as the background property for 
         ///    the canvas. Used to make the window more visually appealing.
-        ///    NOTE:this is currently not functioning as intende and therefore unused. 
+        ///    NOTE:this is currently not functioning as intended and therefore unused. 
         ///    See TODO in MainWindow.xaml.cs
         /// </summary>
         public Brush BrushCanvasBG { get; set; }
@@ -79,22 +90,29 @@ namespace WpfFractals
         public int MaxDepth { get; set; }
 
         /// <summary>
+        /// Gets or sets the drawing speed factor.
+        /// Slows down the drawing process. A depth level is drawn every 'drawSpeed'th 
+        /// tick of CompositionTarget.Rendering
+        /// </summary>
+        public int DrawSpeed { get; set; }
+        
+        /// <summary>
         /// Gets or sets the minimum size of a line segment in pixels (ending point for the recursion, defaults 0)
         /// </summary>
         public int MinSize { get; set; }
 
-        // ---------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the length ratio of child : parent branch.
+        /// </summary>
+        public double LengthScale { get; set; }
 
         /// <summary>
-        /// Slows down the drawing process. A depth level is drawn every 'drawSpeed'th tick of CompositionTarget.Rendering
+        /// Gets or sets the angle +/- delta for child branches
         /// </summary>
-        private int drawSpeed;
+        public double DeltaTheta { get; set; }
+        #endregion
 
-        /// <summary>
-        /// Current fractal drawing depth
-        /// </summary>
-        private int fractalDepth;
-
+        #region Event handlers
         /// <summary>
         /// Handles the click event on the start button, beginning the process of drawing the fractal
         /// </summary>
@@ -103,50 +121,44 @@ namespace WpfFractals
         private void BtnStart_Click(object sender, RoutedEventArgs e)
         {
             this.fractalCanvas.Children.Clear();
-            this.lblProgress.Content = "Drawing...";
-            this.drawSpeed = 0;
+            this.statbarMessage.Text = "Drawing...";
+            this.renderTicks = 0;
             this.fractalDepth = 1;
-            CompositionTarget.Rendering += this.StartAnimation;
+            CompositionTarget.Rendering += this.StartRender;
         }
 
         /// <summary>
-        /// Renders the fractal to screen
+        /// Sets up to render the fractal to screen and starts the rendering process
         /// </summary>
         /// <param name="sender">The object generating the event</param>
         /// <param name="e">EventArgs event arguments</param>
-        private void StartAnimation(object sender, EventArgs e)
+        private void StartRender(object sender, EventArgs e)
         {
-            this.drawSpeed += 1;
-            if (0 == this.drawSpeed % 10)
+            // Track how many times the 'CompositionTarget.Rendering' event fires in order to slow down the render animation.
+            this.renderTicks += 1;
+            if (0 == this.renderTicks % this.DrawSpeed)
             {
                 this.fractalCanvas.Children.Clear();
+
+                // Start the actual rendering
                 this.DrawBinaryTreeBranch(
                     this.fractalCanvas, 
                     this.fractalDepth,
                     new Point(this.fractalCanvas.Width / 2, 0.83 * this.fractalCanvas.Height),
                     0.2 * this.fractalCanvas.Width, 
                     -Math.PI / 2);
-                string str = "Binary Tree - Depth = " + this.fractalDepth.ToString() + ". # of Branches = " + this.fractalCanvas.Children.Count;
-                this.lblProgress.Content = str;
+                this.statbarMessage.Text = "Binary Tree - Depth = " + this.fractalDepth.ToString() + ". # of Branches = " + this.fractalCanvas.Children.Count;
                 this.fractalDepth += 1;
                 if (this.fractalDepth > 10)
                 {
-                    this.lblProgress.Content = "Binary Tree - Depth = 10. Finished. # of Branches = " + this.fractalCanvas.Children.Count;
-                    CompositionTarget.Rendering -= this.StartAnimation;
+                    this.statbarMessage.Text = "Binary Tree - Depth = 10. Finished. # of Branches = " + this.fractalCanvas.Children.Count;
+                    CompositionTarget.Rendering -= this.StartRender;
                 }
             }
         }
+        #endregion
 
-        /// <summary>
-        /// Line segment scaling factor
-        /// </summary>
-        private double lengthScale = 0.75;
-
-        /// <summary>
-        /// Angle offset (+/-) for each child line segment
-        /// </summary>
-        private double deltaTheta = Math.PI / 5;
-
+        #region Methods
         /// <summary>
         /// Draws a single branch of the binary tree and, if not at the recursion limit, calls DrawBinaryTreeBranch for each of this branch's children
         /// </summary>
@@ -181,45 +193,46 @@ namespace WpfFractals
                     canvas,
                     depth - 1,
                     new Point(endX, endY),
-                    length * this.lengthScale,
-                    theta + this.deltaTheta);
+                    length * this.LengthScale,
+                    theta + this.DeltaTheta);
 
                 // draw the -theta segment
                 this.DrawBinaryTreeBranch(
                     canvas,
                     depth - 1,
                     new Point(endX, endY),
-                    length * this.lengthScale,
-                    theta - this.deltaTheta);
+                    length * this.LengthScale,
+                    theta - this.DeltaTheta);
 
-                //// draw the +1/2theta segment
-                //this.DrawBinaryTreeBranch(
-                //    canvas,
-                //    depth - 1,
-                //    new Point(endX, endY),
-                //    length * this.lengthScale,
-                //    theta + this.deltaTheta/2);
+                ////// draw the +1/2theta segment
+                ////this.DrawBinaryTreeBranch(
+                ////    canvas,
+                ////    depth - 1,
+                ////    new Point(endX, endY),
+                ////    length * this.lengthScale,
+                ////    theta + this.deltaTheta/2);
 
-                //// draw the -1/2theta segment
-                //this.DrawBinaryTreeBranch(
-                //    canvas,
-                //    depth - 1,
-                //    new Point(endX, endY),
-                //    length * this.lengthScale,
-                //    theta - this.deltaTheta/2);
+                ////// draw the -1/2theta segment
+                ////this.DrawBinaryTreeBranch(
+                ////    canvas,
+                ////    depth - 1,
+                ////    new Point(endX, endY),
+                ////    length * this.lengthScale,
+                ////    theta - this.deltaTheta/2);
 
-                //// draw a neutral theta segment
-                //this.DrawBinaryTreeBranch(
-                //    canvas,
-                //    depth - 1,
-                //    new Point(endX, endY),
-                //    length * this.lengthScale,
-                //    theta);
+                ////// draw a neutral theta segment
+                ////this.DrawBinaryTreeBranch(
+                ////    canvas,
+                ////    depth - 1,
+                ////    new Point(endX, endY),
+                ////    length * this.lengthScale,
+                ////    theta);
             }
             else
             {
                 return;
             }
         }
+        #endregion
     }
 }
